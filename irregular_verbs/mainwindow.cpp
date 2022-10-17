00785769d0b38f416c6da7f3106626dbb1d83bf3
Fede_T <federicotroncoso144@hotmail.com>
2022-10-17 12:27:16 -0300

    .

--- irregular_verbs/mainwindow.cpp
+++ irregular_verbs/mainwindow.cpp
@@ -14,7 +14,7 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    setWindowIcon(QIcon(":/icon.png"));
+    setWindowIcon(QIcon(":/icon.icon"));
     srand(time(NULL));
     iniciarVentanas();
     inicializar();
